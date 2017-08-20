b974713b93bba57bdd56421a716cef93a6ff0acd
Motaz Saad <motaz.saad@gmail.com>
2017-08-20 19:58:43 +0300

    add trs for new wave files

--- file_list.sh
+++ file_list.sh
@@ -10,3 +10,21 @@ for f in *; do printf "%s\n" " ($f)"; done > ../headlines_10pm_28_07_2017.transc
 cd ..
 cd headlines_10pm_29_07_2017
 for f in *; do printf "%s\n" " ($f)"; done > ../headlines_10pm_29_07_2017.transcription
+
+cd ..
+cd headlines_10am_2_11_2015
+for f in *.wav ; do printf " (%s)\n" "${f}"; done > ../../headlines_10am_2_11_2015.transcription
+
+cd ..
+cd headlines_10pm_14_08_2017
+for f in *.wav ; do printf " (%s)\n" "${f}"; done > ../../headlines_10pm_14_08_2017.transcription
+
+
+cd ..
+cd headlines_10pm_15_08_2017
+for f in *.wav ; do printf " (%s)\n" "${f}"; done > ../../headlines_10pm_15_08_2017.transcription
+
+cd ..
+cd headlines_10pm_19_4_2016
+for f in *.wav ; do printf " (%s)\n" "${f}"; done > ../../headlines_10pm_19_4_2016.transcription
+
